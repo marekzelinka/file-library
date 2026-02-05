2ee2b82eb849ce9e1c8cee2600251e667b83aa9f
Marek Zelinka <mzelinka17@gmail.com>
2026-02-05 10:08:07 +0100

    setup library fixtures

--- test_library.py
+++ test_library.py
@@ -1,8 +1,23 @@
+import pytest
+
 from main import Library
 
 
-def test_add_book() -> None:
+@pytest.fixture
+def library():
+    return Library()
+
+
+@pytest.fixture
+def library_with_books():
     library = Library()
+    library.add_book("To Kill a Mockingbird", "Harper Lee")
+    library.add_book("1984", "George Orwell")
+
+    return library
+
+
+def test_add_book(library) -> None:
     library.add_book("The Great Gatsby", "F. Scott Fitzgerald")
 
     assert library.books == [
@@ -10,30 +25,23 @@ def test_add_book() -> None:
     ]
 
 
-def test_get_book() -> None:
-    library = Library()
+def test_get_book(library) -> None:
     library.add_book("The Great Gatsby", "F. Scott Fitzgerald")
 
     assert library.get_book(0) == "Title: The Great Gatsby, Author: F. Scott Fitzgerald"
 
 
-def test_update_book() -> None:
-    library = Library()
-    library.add_book("The Great Gatsby", "F. Scott Fitzgerald")
-    library.update_book(0, "The Catcher in the Rye", "J.D. Salinger")
+def test_update_book(library_with_books) -> None:
+    library_with_books.update_book(0, "The Catcher in the Rye", "J.D. Salinger")
 
-    assert library.books[0] == {
+    assert library_with_books.books[0] == {
         "title": "The Catcher in the Rye",
         "author": "J.D. Salinger",
     }
 
 
-def test_list_books() -> None:
-    library = Library()
-    library.add_book("To Kill a Mockingbird", "Harper Lee")
-    library.add_book("1984", "George Orwell")
-
-    assert library.list_books() == (
+def test_list_books(library_with_books) -> None:
+    assert library_with_books.list_books() == (
         "Title: To Kill a Mockingbird, Author: Harper Lee\n"
         "Title: 1984, Author: George Orwell"
     )
